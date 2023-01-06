bbd0275161bcac0a33910546e0823128af5dafb1
gpradinett <gpradinett@gmail.com>
2023-01-05 21:46:01 -0500

    ✅ upload file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
-
+"""
+0. Regex-ing
+"""
+from typing import List
 import re
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = f'(?:{separator})({fields})(?:{separator})'
-    return re.sub(pattern, redaction, message)
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """ returns the log message obfuscated """
+    for i in fields:
+        message = re.sub(fr'{i}=.+?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
