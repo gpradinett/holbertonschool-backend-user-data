5b916f026f7dbc3240640347a0d10f5dcf7b8b89
gpradinett <gpradinett@gmail.com>
2023-01-05 22:43:35 -0500

    ✅ upload file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,18 +8,26 @@ import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        constructor method
+        """
+
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        filter values in a log record
+        """
         # NotImplementedError
         return filter_datum(self.fields,
                             self.REDACTION,
