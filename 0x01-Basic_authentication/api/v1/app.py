15858eae09a25b36be5801beb695272d795795bb
gpradinett <gpradinett@gmail.com>
2023-01-10 19:50:58 -0500

    ✅ upload file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,10 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.auth import Auth
-from os import getenv
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
