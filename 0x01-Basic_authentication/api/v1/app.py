fee602dd9e3c1e5ddb2f6dfcb3bcbb46ac8c2f4f
gpradinett <gpradinett@gmail.com>
2023-01-10 19:52:49 -0500

    ✅ upload file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,8 +13,11 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+
 elif os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
