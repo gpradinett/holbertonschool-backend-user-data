cc7636c1ab61a1375bd2dbfded81ba6084fcb016
gpradinett <gpradinett@gmail.com>
2023-01-10 19:13:51 -0500

    ✅ upload file

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import request from flask
+
+
+class Auth:
+    """
+    Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        def require_auth
+        """
+        if not path or not excluded_paths:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        has_wildcard = any(x.endswith("*") for x in excluded_paths)
+        if not has_wildcard:
+            return path not in excluded_paths
+        for e in excluded_paths:
+            if e.endswith("*"):
+                if path.startswith(e[:-1]):
+                    return False
+            if path == e:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        def authorization_header
+        """
+        if request:
+            return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        def curent_user
+        """
+        return None
