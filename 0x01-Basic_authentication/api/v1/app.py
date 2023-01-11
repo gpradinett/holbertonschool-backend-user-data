98df63e64cab7bfbbb5c9589287caabd67e75cba
gpradinett <gpradinett@gmail.com>
2023-01-10 19:45:30 -0500

    ✅ upload file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,19 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.auth import Auth
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -24,7 +32,7 @@ def not_found(error) -> str:
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """
-    for request unauthorized
+    Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
@@ -32,9 +40,24 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """
-
+    Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before():
+    """ Before request.
+    """
+    if auth:
+        paths = ['/api/v1/status/',
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/']
+        if not auth.require_auth(request.path, paths):
+            return
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
