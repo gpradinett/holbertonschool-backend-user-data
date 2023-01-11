d2e1760fbf10d4d43d667b97fb428f3489058707
gpradinett <gpradinett@gmail.com>
2023-01-10 19:49:29 -0500

    ✅ upload file

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -46,18 +46,19 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before():
-    """ Before request.
+def before_request() -> str:
     """
-    if auth:
-        paths = ['/api/v1/status/',
-                 '/api/v1/unauthorized/', '/api/v1/forbidden/']
-        if not auth.require_auth(request.path, paths):
-            return
-        if not auth.authorization_header(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
+    Now the biggest piece is the filtering of each request.
+    For that you will use the Flask method before_request
+    """
+    if auth is not None:
+        if auth.require_auth(request.path, ['/api/v1/status/',
+                             '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+            if auth.authorization_header(request) is None:
+                abort(401)
+
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
