b1d3994d2eb98a224d38fb4657d8878f920123a0
gpradinett <gpradinett@gmail.com>
2023-01-12 20:59:37 -0500

    ✅ upload file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,9 +63,13 @@ def before_request() -> str:
     """
     if auth is not None:
         if auth.require_auth(request.path, ['/api/v1/status/',
-                             '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/']):
+
             if auth.authorization_header(request) is None:
-                abort(401)
+                if auth.session_cookie(request) is None:
+                    abort(401)
 
             if auth.current_user(request) is None:
                 abort(403)
