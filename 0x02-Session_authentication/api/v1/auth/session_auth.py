50ae2d01011f277a64b4c1495d298563751d432c
gpradinett <gpradinett@gmail.com>
2023-01-12 19:04:24 -0500

    ✅ upload file

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module
+"""
+
+from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """
+
+    """
+    pass
